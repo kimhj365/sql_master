075f61ed100eac2391bfe4755602ab79f8ad6984
admin <admin@DESKTOP-VHVMPRV>
2024-01-08 17:51:31 +0900

    20240108 AJAX

---
+++ dev/dev20240108.sql
@@ -0,0 +1,18 @@
+INSERT INTO member
+VALUES ('user4', '4444', '±èÇöÁØ', 'user');
+
+INSERT INTO member
+VALUES ('user5', '5555', '±è»ç¶û', 'user');
+
+INSERT INTO reply
+VALUES (reply_seq.NEXTVAL, 1, '¤·¤·', 'user4', sysdate);
+
+SELECT  replyer, COUNT(*)
+FROM    reply
+GROUP BY replyer;
+
+SELECT      name, COUNT(*)
+FROM        reply r 
+JOIN        member m
+ON          r.replyer = m.id
+GROUP BY    name;
