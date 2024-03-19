7f05a2b7662056cb6688a4796149f26a2e5d7bf7
admin <admin@DESKTOP-VHVMPRV>
2024-03-19 18:12:39 +0900

    20240319 Spring 트랜잭션

---
+++ spring/20240319.sql
@@ -0,0 +1,10 @@
+-- 20240319 Spring
+
+CREATE TABLE tbl_sample1( col1 VARCHAR2(500) );
+CREATE TABLE tbl_sample2( col2 VARCHAR2(10) );
+
+SELECT * FROM tbl_sample1;
+SELECT * FROM tbl_sample2;
+
+DELETE tbl_sample1;
+DELETE tbl_sample2;
